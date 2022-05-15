ad09ca05dc967bd1f3aba0baf6d31a877947249a
Sik0M0D# <98068234+Sik0M0D3@users.noreply.github.com>
2022-05-15 00:17:33 -0300

    Create 6ozkzl.theme.h

---
+++ themes/6ozkzl.theme.h
@@ -0,0 +1,29 @@
+static const char *colorname[] = {
+  /* 8 normal colors */
+  "#0B0D0E",
+  "#592F91",
+  "#87B52B",
+  "#B8C132",
+  "#3336C6",
+  "#526839",
+  "#8E4D2B",
+  "#C09F9A",
+
+  /* 8 bright colors */
+  "#866F6B",
+  "#592F91",
+  "#87B52B",
+  "#B8C132",
+  "#3336C6",
+  "#526839",
+  "#8E4D2B",
+  "#C09F9A",
+
+  [255] = 0,
+
+  /* more colors can be added after 255 to use with DefaultXX */
+  "#cccccc",
+  "#555555",
+  "#C09F9A",  /* default foreground colour */
+  "#0B0D0E", /* default background colour */
+};
